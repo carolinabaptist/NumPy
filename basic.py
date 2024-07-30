6aee732f41fca144570c1cdbb7848df0ba6fff91
Ana Carolina Baptista <119694053+carolinabaptist@users.noreply.github.com>
2024-07-30 10:52:07 +0100

    Update basic.py

--- basic.py
+++ basic.py
@@ -12,9 +12,31 @@ def getHelp():
 
 #Write a NumPy program to test whether none of the elements of a given array are zero. 
 
-a = np.array([[1, 2, 3, 4]])
 def isZero(a):
     print(np.all(a))
     return
 
 #Write a NumPy program to test if any of the elements of a given array are non-zero. 
+
+def isNonZero(b):
+    bool_array = b != 0
+    if False in bool_array:
+        return True
+    return False
+
+def isNonZeroTwo(b):    
+    return np.any(b)
+
+#Write a  NumPy program to test a given array element-wise for finiteness (not infinity or not a number).
+
+#array = np.array([1, 2, np.inf, -np.inf, 0, np.nan])
+
+def isInf(array):
+    if True in np.isinf(array):
+        return True
+    return False
+
+def isNan(array):
+    if True in np.isnan(array):
+        return True
+    return False
